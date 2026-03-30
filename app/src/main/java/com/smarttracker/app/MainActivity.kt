8fc25a9ef6a57438dd0cc5bfa024fce066e3cda9
Rohit19082007 <126911625+Rohit19082007@users.noreply.github.com>
2026-03-31 01:58:38 +0530

    MainActivity.kt

--- app/src/main/java/com/smarttracker/app/MainActivity.kt
+++ app/src/main/java/com/smarttracker/app/MainActivity.kt
@@ -1,1 +1,27 @@
+package com.smarttracker.app
 
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val amountInput = findViewById<EditText>(R.id.amountInput)
+        val addBtn = findViewById<Button>(R.id.addBtn)
+        val listView = findViewById<ListView>(R.id.listView)
+
+        val list = ArrayList<String>()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        listView.adapter = adapter
+
+        addBtn.setOnClickListener {
+            val amount = amountInput.text.toString()
+            list.add("₹$amount")
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
